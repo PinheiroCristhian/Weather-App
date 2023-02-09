89359ff99e8928f908c3f053bf80306554d56817
Cristhian Pinheiro <cristhianm.pinheiro@gmail.com>
2023-02-08 23:24:47 -0300

    Fixed error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import APICall from "./modules/APICall";
     const cityName = DOM().searchField;
 
     button.addEventListener('click', async () => {    
-        if (!cityName.value || cityName.value[0] === ' ') return null;
+        if (!cityName.value || cityName.value[0] === ' ') return alert('Enter A Valid City');
         
         const cityData = await APICall(cityName.value);
         display(cityData.name, cityData.sys.country, cityData.main.temp, cityData.main.humidity, cityData.weather[0].description);
